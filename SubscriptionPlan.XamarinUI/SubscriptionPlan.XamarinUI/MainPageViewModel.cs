005f214fd70420b37141188ef27f580504985e1f
Alexandre Lima <alexandreslima@outlook.com>
2020-03-09 12:44:32 -0300

    Add project files.

---
+++ SubscriptionPlan.XamarinUI/SubscriptionPlan.XamarinUI/MainPageViewModel.cs
@@ -0,0 +1,47 @@
+﻿using SubscriptionPlan.XamarinUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+
+namespace SubscriptionPlan.XamarinUI
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public MainPageViewModel()
+        {
+            PlanList = new ObservableCollection<SubscriptionPlanModel>
+            {
+                new SubscriptionPlanModel
+                {
+                    Title = "Base",
+                    Description = "Description Plan",
+                    Price = 10
+                },
+                new SubscriptionPlanModel
+                {
+                    Title = "Medium",
+                    Description = "Description Plan",
+                    Price = 30
+                },
+                new SubscriptionPlanModel
+                {
+                    Title = "Ultimate",
+                    Description = "Description Plan",
+                    Price = 100
+                },
+                new SubscriptionPlanModel
+                {
+                    Title = "Ultimate",
+                    Description = "Description Plan",
+                    Price = 100
+                }
+            };
+        }
+
+        public ObservableCollection<SubscriptionPlanModel> PlanList { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ SubscriptionPlan.XamarinUI/SubscriptionPlan.XamarinUI/Models/SubscriptionPlan.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SubscriptionPlan.XamarinUI.Models
+{
+    public class SubscriptionPlanModel
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string PricePresentation { get { return Price.ToString("C"); } }
+    }
+}
